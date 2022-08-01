204db312843a92209b316f32ac1695a391179f64
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-08-01 23:44:24 +0400

    node js

---
+++ backend-test/modules/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    fName: {
+      type: String,
+      require: true,
+    },
+    lName: {
+      type: String,
+      require: true,
+    },
+    note: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestemps: true }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
--- backend-test/server.js
+++ backend-test/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./modules/blog");
 
 const app = express();
+
+const dbURL =
+  "mongodb+srv://ceyhun:23042000@cluster0.jn3bg.mongodb.net/furniture?retryWrites=true&w=majority";
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.listen("3000");
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(dbURL, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    app.listen("3000");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -20,3 +35,42 @@ app.get("/product", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  console.log("--------------------");
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      console.log(result + "salam");
+      res.redirect("/admin");
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/admin", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render("admin", { blogs: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+// app.get("/add", (req, res) => {
+//   const blog = new Blog({
+//     title: "birde salam",
+//     desc: "ne var ne yox",
+//   });
+//   blog
+//     .save()
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((err) => {
+//       console.log(err + "errrrror");
+//     });
+// });
